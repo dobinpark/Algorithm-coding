05c0a76188139fadfbd4a39d3b58e41113b4ac53
Park-Gun <ff5026@naver.com>
2023-02-20 22:09:08 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,15 +1,8 @@
 class Solution {
     public int[] solution(int money) {
         int[] answer = new int[2];
-        int americano = 5500;                 // 아메리카노 가격
-        int total = money / americano; // 최대 마실 수 있는 잔 수
-        
-        for (int i = 0; i < total; i++) {
-            money -= americano;
-        }
-        
-        answer[0] = total;
-        answer[1] = money;
+        answer[0] = money / 5500;
+        answer[1] = money % 5500;
         
         return answer;
     }
