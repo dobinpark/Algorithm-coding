20bd2183dcdabf4c79b4bb38416f320d781839d2
Park-Gun <ff5026@naver.com>
2023-04-05 01:11:36 +0900

    [D1] Title: 신문 헤드라인, Time: 129 ms, Memory: 20,836 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        String t = sc.next();
+
+        String str = t.toUpperCase();
+
+        System.out.println(str);
+    }
+}
