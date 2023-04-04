c3e35bae43e55b5199e98dead5fbd32885a2b219
Park-Gun <ff5026@naver.com>
2023-04-04 23:00:05 +0900

    [D1] Title: 아주 간단한 계산기, Time: 128 ms, Memory: 20,516 KB -BaekjoonHub

---
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+    }
+}
