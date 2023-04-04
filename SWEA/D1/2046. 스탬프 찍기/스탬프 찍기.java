004f32beca84d92943ab5e6d259d90b3fb44c8bd
Park-Gun <ff5026@naver.com>
2023-04-05 01:06:47 +0900

    [D1] Title: 스탬프 찍기, Time: 133 ms, Memory: 20,396 KB -BaekjoonHub

---
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 1; i <= t; i++) {
+            System.out.print("#");
+        }
+    }
+}
